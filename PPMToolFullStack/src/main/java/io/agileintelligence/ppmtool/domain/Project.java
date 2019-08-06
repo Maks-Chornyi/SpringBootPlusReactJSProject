20e8654079c27b5194b7e57a4a8171586198396e
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-08-06 20:35:10 +0300

    added validation to all fields in Project class

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,13 +14,21 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Description should be beetwen 4 and 5 chars")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project desc is required")
     private String description;
-    private Date start_date;
-    private Date end_date;
-    private Date created_At;
-    private Date updated_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")    private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")    private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")    private Date updated_At;
 
 
     public Project() {
