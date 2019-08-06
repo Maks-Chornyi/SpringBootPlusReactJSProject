c12437c0af54c41c684ae92d2fe7fe6af919b293
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-08-06 20:26:34 +0300

    refactored code

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/controller/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/controller/ProjectController.java
@@ -20,8 +20,8 @@ public class ProjectController {
 
     @PostMapping
     public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
-        Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 
 
