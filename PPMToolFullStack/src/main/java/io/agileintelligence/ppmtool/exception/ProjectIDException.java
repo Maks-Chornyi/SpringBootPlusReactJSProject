3337b408af96434ac396b9499f10480c286b211f
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-08-09 19:17:47 +0300

    added validations and exception message for project with unexisting id

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/exception/ProjectIDException.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProjectIDException extends RuntimeException {
+
+    public ProjectIDException(String message) {
+        super(message);
+    }
+}
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/service/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.service;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exception.ProjectIDException;
 import io.agileintelligence.ppmtool.repo.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,20 @@ public class ProjectService {
     }
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepo.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepo.save(project);
+        } catch (Exception e) {
+            throw new ProjectIDException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByProjectIdentifier(String projectIdentifier) {
+        Project project =  projectRepo.findProjectByProjectIdentifier(projectIdentifier.toUpperCase());
+        if (project == null) {
+            throw new ProjectIDException("Project with ID '" + projectIdentifier + "' does not exist");
+        }
+        return project;
     }
 
 
